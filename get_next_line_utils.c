83abe169db81684ef0bcb9e151b58e2edf221c8c
hphanixa <77008970+hphanixa@users.noreply.github.com>
2021-02-04 16:45:56 +0100

    Create get_next_line_utils.c

---
+++ get_next_line_utils.c
@@ -0,0 +1,53 @@
+#include "get_next_line.h"
+
+size_t		ft_strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+void		*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char		*d;
+	const char	*s;
+	size_t		i;
+
+	d = (char *)dst;
+	s = (const char *)src;
+	i = -1;
+	if (!d && !s)
+		return (NULL);
+	if (d < s)
+	{
+		while (++i < len)
+			d[i] = s[i];
+	}
+	else
+	{
+		while (len-- > 0)
+			d[len] = s[len];
+	}
+	return (d);
+}
+
+char		*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len;
+
+	if (!s1 && !s2)
+		return (0);
+	len = (ft_strlen(s1) + ft_strlen(s2));
+	if (!(str = malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	ft_memmove(str, s1, ft_strlen(s1));
+	ft_memmove(str + ft_strlen(s1), s2, ft_strlen(s2));
+	str[len] = '\0';
+	return (str);
+}
