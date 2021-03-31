94d630e1c9ac561e8e48799d056c907661dd9f5c
hphanixa <77008970+hphanixa@users.noreply.github.com>
2021-03-31 15:30:05 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,17 +1,21 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-//#define BUFFER_SIZE 78
+
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 2
+# endif
+
 int		get_next_line(int const fd, char **line);
-size_t	ft_strlen(const char *s);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr_and_cpy(char *s, int c);
+int		ft_strlen(const char *s);
+char	*ft_join(char const *s1, char const *s2);
+char	*ft_stock(char *s);
 int		is_return(char *str);
-char	*ft_strdup(char *s1);
+char	*ft_line(char *s1);
+int		ft_countbeforen(char *str);
 
 #endif
