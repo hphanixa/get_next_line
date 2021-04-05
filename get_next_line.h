53967cb0459aafb590197e582b97cdb87833a84a
hphanixa <77008970+hphanixa@users.noreply.github.com>
2021-04-05 15:47:06 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -14,8 +14,8 @@ int		get_next_line(int const fd, char **line);
 int		ft_strlen(const char *s);
 char	*ft_join(char const *s1, char const *s2);
 char	*ft_stock(char *s);
-int		is_return(char *str);
-char	*ft_line(char *s1);
-int		ft_countbeforen(char *str);
+int		is_eol(const char *str);
+char	*ft_line(const char *s1);
+int		ft_countbeforen(const char *str);
 
 #endif
